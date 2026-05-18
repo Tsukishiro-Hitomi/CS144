b27ea44361a715026e3cb49dd5b2594320b52104
zhaoxinrui <24307140096@fudan.m.edu.cn>
2026-05-18 10:40:02 +0800

    checkpoint2 modified

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -34,10 +34,9 @@ void TCPReceiver::receive( TCPSenderMessage message )
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
+  // Send the ACK segment
   TCPReceiverMessage msg;
-  
-  // 1. 设置滑动窗口大小
-  // 强制转换可用容量，并且由于 uint16_t 最大只有 65535，需要给它设个上限
+
   uint64_t capacity = writer().available_capacity();
   msg.window_size = capacity > UINT16_MAX ? UINT16_MAX : capacity;
   
@@ -45,13 +44,12 @@ TCPReceiverMessage TCPReceiver::send() const
     msg.RST = true;
   }
 
-  // 2. 如果收到过 SYN 包，计算要发给对面的期待序列号 (ackno)
   if ( zero_point_.has_value() ) {
-      // 应该是已经成功 pushed 到 stream 中的字节 + 1个 SYN 标志位
-      // 别忘了，如果有 FIN 还要再 + 1 
       uint64_t next_absolute_seqno = writer().bytes_pushed() + 1;
+
+      // FIN flag is included in the sequence number but not pushed into byte stream.
       if ( writer().is_closed() ) {
-          next_absolute_seqno += 1; // FIN flag 被签收后累加
+          next_absolute_seqno += 1;
       }
       msg.ackno = Wrap32::wrap( next_absolute_seqno, zero_point_.value() );
   }
